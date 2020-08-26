5e5d69679d64a83deab9133062f9642012b89ad0
Patrick Lederer <Patrick.Lederer@brz.eu>
2020-08-26 11:14:33 +0200

    initial

---
+++ Services/BarcodeScannerService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using ZXing;
+
+
+namespace BarcodeScanner.Services
+{
+    public class BarcodeScannerService
+    {
+        public async Task<string> StreamToBase64StringAsync(MemoryStream stream)
+        {
+            stream.Seek(0, 0);
+            var base64 = Convert.ToBase64String(stream.ToArray());
+            return base64;
+        }
+
+        public async Task<string> GetBarcodeFromFileAsync(MemoryStream stream)
+        {
+            try
+            {
+                var coreCompatReader = new BarcodeReader();
+                stream.Seek(0, 0);
+                var coreCompatImage = (Bitmap) Image.FromStream(stream);
+
+                var coreCompatResult = coreCompatReader.Decode(coreCompatImage);
+                return coreCompatResult != null ? coreCompatResult.Text : "empty";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return e.ToString();
+            }
+
+        }
+    }
+}
